4bfac76a182b5ccab48ff2fa6bb5a967afcaebcf
tanishq1134 <151707747+tanishq1134@users.noreply.github.com>
2025-10-05 11:43:09 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+console.log(2>1)
+// >=,<=,==,!=
+
+console.log("2">1)//give true as "2" is converted into numbers
+
+//but results are not predictable  so it is better if you convert them
+//into same datatype
+console.log(null==0)//falsex
+console.log(null>0);//false
+console.log(null>=0);//true
+
+console.log(undefined==0)//false
+
+// === this checks values and data types both 
+console.log("2"===2);//false
+
